6288a59ba2f76b3953381c53a7e0f3a99f50d353
orez1589 <erez@keynetik.net>
2015-03-18 22:01:07 +0200

    first commit

---
+++ Slot Machine/Factory.swift
@@ -0,0 +1,9 @@
+//
+//  Factory.swift
+//  Slot Machine
+//
+//  Created by Erez porat on 13/03/15.
+//  Copyright (c) 2015 erez. All rights reserved.
+//
+
+import Foundation
---
+++ Slot Machine/SlotBrain.swift
@@ -0,0 +1,9 @@
+//
+//  SlotBrain.swift
+//  Slot Machine
+//
+//  Created by Erez porat on 17/03/15.
+//  Copyright (c) 2015 erez. All rights reserved.
+//
+
+import Foundation
